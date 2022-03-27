73105ad8b45558454977cb3489eed2694751fb55
jerrynim <jerrynim@zigbang.com>
2022-03-27 23:27:23 +0900

    next로  재시작

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Head from "next/head"
+import styled from "styled-components"
+
+const index: React.FC = () => {
+    return (
+        <>
+            <Head>
+                <title>Buy The Dip</title>
+            </Head>
+        </>
+    )
+}
+
+export default index
